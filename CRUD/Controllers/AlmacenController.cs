bb6fb83167ee3cafc9cd91742cf4695abb68dc76
AD <AD@AD>
2020-09-20 18:55:06 -0500

    Agregar archivos de proyecto.

---
+++ CRUD/Controllers/AlmacenController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD.Models;
+
+namespace CRUD.Controllers
+{
+    public class AlmacenController : Controller
+    {
+        private CURSOMVCEntities db = new CURSOMVCEntities();
+
+        // GET: Almacen
+        public ActionResult Index()
+        {
+            return View(db.Almacen.ToList());
+        }
+
+        // GET: Almacen/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacen/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Almacen/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlmacenID,AlmacenDescripcion,FechaInicio,FechaFin,Entero,Decimal")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Almacen.Add(almacen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(almacen);
+        }
+
+        // GET: Almacen/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacen/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AlmacenID,AlmacenDescripcion,FechaInicio,FechaFin,Entero,Decimal")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(almacen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacen/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacen/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Almacen almacen = db.Almacen.Find(id);
+            db.Almacen.Remove(almacen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
